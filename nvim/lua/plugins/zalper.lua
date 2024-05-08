29df1073a2b365bc3f75aee8965ce131b43b7b25
zalper <auzgec@gmail.com>
2024-05-08 22:33:11 +0300

    update modules

--- nvim/lua/plugins/zalper.lua
+++ nvim/lua/plugins/zalper.lua
@@ -264,4 +264,18 @@ return {
       })
     end,
   },
+
+  -- neo-tree.nvim
+
+{
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+},
+
 }
